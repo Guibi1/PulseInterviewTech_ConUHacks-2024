b73a36b51edab438dc9b6280b725bdf4a44b9647
Laurent Stéphenne <laurent@guibi.dev>
2024-01-20 15:54:40 -0500

    Bit of layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,44 +1,31 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+    import "../app.postcss";
+    import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+    import Logo from "./Logo.svelte";
 </script>
 
-<!-- App Shell -->
 <AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
+    <svelte:fragment slot="header">
+        <AppBar>
+            <div slot="lead" class="flex items-center">
+                <Logo />
+                <strong class="text-xl uppercase">Pulse Interview</strong>
+            </div>
+
+            <svelte:fragment slot="trail">
+                <a class="btn btn-sm variant-ghost-surface" href="/"> Take the interview </a>
+                <a class="btn btn-sm variant-ghost-surface" href="/generate-cv"> Generate a CV </a>
+                <a
+                    class="btn btn-sm variant-ghost-surface"
+                    href="https://github.com/Guibi1/PulseInterviewTech_ConUHacks-2024"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    GitHub
+                </a>
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+
+    <slot />
 </AppShell>
