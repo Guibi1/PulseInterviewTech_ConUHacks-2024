f514742bc197d424fa286d340e64a3fff6a79090
Laurent Stéphenne <laurent@guibi.dev>
2024-01-20 13:33:39 -0500

    Fixed weird error

--- apps/web/src/lib/zod.ts
+++ apps/web/src/lib/zod.ts
@@ -4,12 +4,12 @@ export const pdfUploadSchema = z.object({
     pdf: z
         .custom<File>()
         .refine((f) => f instanceof File && f.size < 10000, "Max 10Kb upload size.")
-        .refine((f) => f.type === "application/pdf", "Must be a PDF."),
+        .refine((f) => f && f.type === "application/pdf", "Must be a PDF."),
 });
 
 export const videoUploadSchema = z.object({
     pdf: z
         .custom<File>()
         .refine((f) => f instanceof File && f.size < 100000, "Max 100Kb upload size.")
-        .refine((f) => f.type === "video/*", "Must be a PDF."),
+        .refine((f) => f && f.type === "video/*", "Must be a PDF."),
 });
